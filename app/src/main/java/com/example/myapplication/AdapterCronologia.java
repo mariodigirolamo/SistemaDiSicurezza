889241e94211bc41829f81b0f443cbb218b9de40
mario <iosonosura@hotmail.it>
2018-06-20 15:12:22 +0200

    Prima modifica alla grafica del grafico v2.2

---
+++ app/src/main/java/com/example/myapplication/AdapterCronologia.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AdapterCronologia extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+    private final String[] crono;
+
+    public AdapterCronologia(Context context, String[] values, String[] crono) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+        this.crono = crono;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.righe_cronologiaf, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.id);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icona);
+
+        String s = values[position];
+        String c = crono[position];
+
+        textView.setText(crono[position]);
+
+        return rowView;
+    }
+}
--- app/src/main/java/com/example/myapplication/Grafico.java
+++ app/src/main/java/com/example/myapplication/Grafico.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Grafico extends AppCompatActivity {
 
-    private TextView vprova;
-    private Button vfuga;
+    private TextView vstatom;
+    private TextView vstatof;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -17,21 +18,28 @@ public class Grafico extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
+        vstatom = findViewById(R.id.statomovimento);
+        vstatof = findViewById(R.id.statofumo);
+
+        ListView listafumo = (ListView) findViewById(R.id.fumolist);
+        ListView listamov = (ListView) findViewById(R.id.movlist);
+
         Bundle extras = getIntent().getExtras();
         if(extras != null){
 
 
-            int datom1 = extras.getInt("datom1");
-            int datom2 = extras.getInt("datom2");
-            int datom3 = extras.getInt("datom3");
-            int datom4 = extras.getInt("datom4");
-            int datom5 = extras.getInt("datom5");
+            String datom1 = String.valueOf(extras.getInt("datom1"));
+            String datom2 = String.valueOf(extras.getInt("datom2"));
+            String datom3 = String.valueOf(extras.getInt("datom3"));
+            String datom4 = String.valueOf(extras.getInt("datom4"));
+            String datom5 = String.valueOf(extras.getInt("datom5"));
 
-            int dato1 = extras.getInt("dato1");
-            int dato2 = extras.getInt("dato2");
-            int dato3 = extras.getInt("dato3");
-            int dato4 = extras.getInt("dato4");
-            int dato5 = extras.getInt("dato5");
+            String dato1 = String.valueOf(extras.getInt("dato1"));
+            String dato2 = String.valueOf(extras.getInt("dato2"));
+            String dato3 = String.valueOf(extras.getInt("dato3"));
+            String dato4 = String.valueOf(extras.getInt("dato4"));
+            String dato5 = String.valueOf(extras.getInt("dato5"));
+
 
             String dataf1 = extras.getString("dataf1");
             String dataf2 = extras.getString("dataf2");
@@ -45,26 +53,17 @@ public class Grafico extends AppCompatActivity {
             String datam4 = extras.getString("datam4");
             String datam5 = extras.getString("datam5");
 
-            vfuga = findViewById(R.id.fuga);
-            vprova = findViewById(R.id.prova);
+            String [] datifumo = new String [] {dato1, dato2, dato3, dato4, dato5};
+            String [] datimovimento = new String [] {datom1, datom2, datom3, datom4, datom5};
 
-            String datos1 = String.valueOf(dato1);
-            String datos2 = String.valueOf(dato2);
-            String datos3 = String.valueOf(dato3);
-            String datos4 = String.valueOf(dato4);
-            String datos5 = String.valueOf(dato5);
+            String [] datefumo = new String [] {dataf1, dataf2, dataf3, dataf4, dataf5};
+            String [] datemov = new String [] {datam1, datam2, datam3, datam4, datam5};
 
-            vprova.setText("Ultime segnalazioni sul fumo:"+"\n"+dataf1+"\n"+dataf2+"\n"+dataf3+"\n"+dataf4+"\n"+dataf5+"\n"+"\n"+
-                            "Ultime segnalazione sul movimento:"+"\n"+datam1+"\n"+datam2+"\n"+datam3+"\n"+datam4+"\n"+datam5);
+            AdapterCronologia adapterf = new AdapterCronologia(this, datifumo, datefumo);
+            AdapterCronologiaMov adaptermov = new AdapterCronologiaMov(this, datimovimento, datemov);
 
-            vfuga.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    finish();
-                }
-            });
-
+            listafumo.setAdapter(adapterf);
+            listamov.setAdapter(adaptermov);
 
         }
 
