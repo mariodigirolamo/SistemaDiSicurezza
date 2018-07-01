05df7accdffa003b459a5cfbcb3e4cc8bfa3d9ac
mario <iosonosura@hotmail.it>
2018-07-01 17:51:46 +0200

    Login mail

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.text.style.EasyEditSpan;
@@ -11,14 +12,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
 
     private final String TAG = "LIFE_LOGIN";
 
-    private final String cod = "cart321";
+    private EditText vmail;
+    private EditText vpasslog;
 
-    private Button vbutt;
-    private EditText vpass;
+    private Button vsignin;
+    private Button vsignup;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -27,12 +37,41 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        vsignin = findViewById(R.id.signin);
+
+        vmail = findViewById(R.id.mail);
+
+        vpasslog = findViewById(R.id.passlog);
+
+        vsignup = findViewById(R.id.signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        vsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    registerUser(mAuth, vmail.getText().toString(), vpasslog.getText().toString());
+                } catch (Exception e){}
+
+            }
+        });
+
+        vsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    loginUser(mAuth, vmail.getText().toString(), vpasslog.getText().toString());
+                } catch (Exception es){}
+
+            }
+        });
+
         Log.v(TAG, "OnCreate");
 
-        vbutt = findViewById(R.id.butt);
-        vpass = findViewById(R.id.pass);
-
-        vbutt.setOnClickListener(new View.OnClickListener() {
+        /*vbutt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -42,12 +81,16 @@ public class Login extends AppCompatActivity {
                 else{Toast.makeText(getApplicationContext(),"Il codice inserito è errato.", Toast.LENGTH_LONG).show();}
 
             }
-        });
+        });*/
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
 
         Log.v(TAG, "onStart");
 
@@ -86,5 +129,61 @@ public class Login extends AppCompatActivity {
         super.onRestart();
 
         Log.v(TAG, "onRestart");
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+    private void registerUser (FirebaseAuth bauth, String email, String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(getApplicationContext(), "Complimenti, registrazione avvenuta con successo! Clicca Login per accedere", Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "E-mail o password errata.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    private void loginUser (FirebaseAuth cauth, String email, String password){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
