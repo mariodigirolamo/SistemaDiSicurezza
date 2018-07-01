6a6e4d5c0b914ec7b50873adab10d760baf30b2f
mario <iosonosura@hotmail.it>
2018-07-01 11:07:23 +0200

    Aggiunta ricerca luoghi su mappa

--- app/src/main/java/com/example/myapplication/Mappa.java
+++ app/src/main/java/com/example/myapplication/Mappa.java
@@ -2,8 +2,14 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.telecom.Call;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -14,19 +20,48 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Mappa extends AppCompatActivity implements OnMapReadyCallback{
 
+    private String TAG = "MAP_LIFE";
+
     private MapView vMappa;
     private GoogleMap mGoogleMap;
+    private CardView vCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_mappa);
 
+        vCard = findViewById(R.id.Card);
+
         vMappa = findViewById(R.id.vMappa);
 
         vMappa.onCreate(savedInstanceState);
 
         vMappa.getMapAsync(this);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+
+                mGoogleMap.clear();
+
+                mGoogleMap.addMarker(new MarkerOptions().position(place.getLatLng()));
+
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 10));
+
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
 
     }
 
